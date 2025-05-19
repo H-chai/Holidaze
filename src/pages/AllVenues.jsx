c908c0ac102d43a6123f7dfffd2a3e09fad46565
H-chai <143179912+H-chai@users.noreply.github.com>
2025-05-19 11:23:23 +0200

    Use correct path name

--- src/pages/AllVenues.jsx
+++ src/pages/AllVenues.jsx
@@ -6,7 +6,7 @@ import StarIcon from '@mui/icons-material/Star';
 import { useState, useEffect } from 'react';
 import ReactPaginate from 'react-paginate';
 import { SearchResultSection } from '../components/homePage/SearchResultSection';
-import { HeroSection } from '../components/homePage/heroSection';
+import { HeroSection } from '../components/homePage/HeroSection';
 
 /**
  * AllVenues - A component that fetches and displays a list of all venues.
