3aa2b116c2c203308f87a304ae6f518f3ebcd6b1
H-chai <143179912+H-chai@users.noreply.github.com>
2025-05-19 11:20:45 +0200

    Use correct path name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-import { HeroSection } from '../components/homePage/heroSection';
+import { HeroSection } from '../components/homePage/HeroSection';
 import { MostPopularSection } from '../components/homePage/MostPopularSection';
 import { NewlyAddedSection } from '../components/homePage/NewlyAddedSection';
 import { TrendingDestinationSection } from '../components/homePage/TrendingDestinationSection';
